9da6d3da8bf3fa8294db80d1e581f366560b1025
Sami Munir <sami.munir2001@gmail.com>
2024-12-06 03:17:21 -0500

    implementing Cli structure to handle Cli commands

--- Projects/Rust Projects/todo-app/src/main.rs
+++ Projects/Rust Projects/todo-app/src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, Subcommand};
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -5,6 +6,22 @@ struct Task {
     id: usize,
     description: String,
     done: bool,
+}
+
+#[derive(Parser)]
+#[command(name = "Todo App")]
+#[command(about = "A simple command-line to-do list app", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {description: String},
+    List,
+    Done {id: usize},
+    Remove {id: usize},
 }
 
 fn main() {
