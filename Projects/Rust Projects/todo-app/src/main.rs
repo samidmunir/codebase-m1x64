d0b489424018a5db7239e51b563c6859352ac262
Sami Munir <sami.munir2001@gmail.com>
2024-12-06 03:13:00 -0500

    creating Task structure

--- Projects/Rust Projects/todo-app/src/main.rs
+++ Projects/Rust Projects/todo-app/src/main.rs
@@ -1,3 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    id: usize,
+    description: String,
+    done: bool,
+}
+
 fn main() {
     println!("\nRusty Todo App\n");
 }
