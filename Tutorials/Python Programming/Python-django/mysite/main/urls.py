5b55553e340bba3b3fbc9c2ebdd199f2aee259f6
Sami Munir <sami.munir2001@gmail.com>
2024-12-09 07:37:58 -0500

    creating first application view and url

---
+++ Tutorials/Python Programming/Python-django/mysite/main/urls.py
@@ -0,0 +1,16 @@
+"""
+    Paths to different webpages (endpoints)
+    - URLS to different views...
+    
+    Which view to go to based on URL path
+"""
+
+from django.urls import path
+from . import views
+
+"""
+    root direction -> views.index page (name is index)
+"""
+urlpatterns = [
+    path('', views.index, name = 'index'),
+]
--- Tutorials/Python Programming/Python-django/mysite/main/views.py
+++ Tutorials/Python Programming/Python-django/mysite/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+"""
+    Store all the views for the application.
+    - Like a webpage
+    - holds all the code that serves HTTP requests
+"""
 
 # Create your views here.
+
+def index(response):
+    return HttpResponse('<h1>Hello world! - index</h1>')
