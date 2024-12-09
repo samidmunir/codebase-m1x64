9d5854db97313b1af1191d085a6fc75c187d5f0f
Sami Munir <sami.munir2001@gmail.com>
2024-12-09 07:42:28 -0500

    linked app with django project/admin

--- Tutorials/Python Programming/Python-django/mysite/mysite/urls.py
+++ Tutorials/Python Programming/Python-django/mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # linking app URLS with the admin (project)
+    path("", include("main.urls")),
 ]
